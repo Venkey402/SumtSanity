f678d28fc90f2b7048284a3c16e89c341b533e1b
venkey402 <venkey402@gmail.com>
2021-11-30 10:35:42 +0530

    comment ...

---
+++ src/test/java/com/Sumtotal/testCases/xlsReader.java
@@ -0,0 +1,15 @@
+package com.Sumtotal.testCases;
+
+import org.xml.sax.XMLReader;
+
+public class xlsReadertest {
+
+
+	public static void main(String[] args) {
+
+		XMLReader reader = new XMLReader("C:\\Venkat_Personal\\Interview\\Material\\test.xlsx");
+		
+		
+	}
+
+}
